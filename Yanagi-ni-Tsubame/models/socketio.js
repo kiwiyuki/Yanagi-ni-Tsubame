f15d458170981de23c32ee4ab08b3b894f97ad93
ほつれ <cerceris326@gmail.com>
2014-09-22 18:07:43 +0900

    初期化位置の変更

--- Yanagi-ni-Tsubame/models/socketio.js
+++ Yanagi-ni-Tsubame/models/socketio.js
@@ -1,9 +1,8 @@
-var socketio = require("socket.io");
-var go = require("./gameObject");
-var session = require("express-session").Session;
-var COOKIE_SECRET = "secret-session";
-var COOKIE_KEY    = "sid";
-
+var socketio = require('socket.io');
+var go = require('./gameObject');
+var session = require('express-session').Session;
+var COOKIE_SECRET = 'secret-session';
+var COOKIE_KEY    = 'sid';
 
 var players = [];
 var enemys = [];
@@ -12,26 +11,28 @@ var items = [];
 function socketio (server) {
 	var io = socketio.listen(server);
 	io.use(function　(socket, next) {
-		var cookie = require("cookie").parse(socket.request.headers.cookie);
+		var cookie = require('cookie').parse(socket.request.headers.cookie);
 		cookie = require('cookie-parser/lib/parse').signedCookies(cookie, COOKIE_SECRET);
 		var sessionID = cookie[COOKIE_KEY];
 		console.log(sessionID);
 	});
+
 	// サーバー接続処理
-	io.sockets.on('connection', function(socket) {
-		var p = new go.Player(socket.id,0,0,"");
+	io.sockets.on('connection', function (socket) {
+		var p = new go.Player(socket.id, 0, 0, '');
 		players.push(p);
 		console.log(socket);
+
 		// 初回データ送信
-		socket.json.emit('first_message', { player:p, players: players, enemys: enemys, items:items });
+		socket.json.emit('first_message', { player: p, players: players, enemys: enemys, items: items }, function() {});
 		
 		// 各クライアントのデータ受信
-		socket.json.on("client_data", function (data) {
+		socket.json.on('client_data', function (data) {
 
 		});
 
 		// 切断処理
-		socket.on("disconnect", function() {
+		socket.on('disconnect', function() {
 			for(var i = 0; i < players.length; i++) {
 				if(players.id === socket.id) {
 					players.splice(i,1);
