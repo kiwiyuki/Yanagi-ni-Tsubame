8e8f398345870595cc624c0feddaf03576ba96d7
kiwiyuki <kiwi.kyuui@gmail.com>
2014-09-22 16:42:11 +0900

    gameObjectの追加

---
+++ Yanagi-ni-Tsubame/models/gameObject.js
@@ -0,0 +1,45 @@
+var Player = function(_id, _x, _y, _color) {
+	return {
+		id: _id,
+		x: _x,
+		y: _y,
+		shots: [],
+		hp: 0,
+		state: {},
+		color: _color
+	};
+};
+
+var Shots = function() {
+	// return {
+	// 	id: 
+	// };
+};
+
+
+var Enemy = function(_id, _x, _y, _type) {
+	return {
+		id: _id,
+		x: _x,
+		y: _y,
+		type: _type,
+		hp: 0,
+		counter: 0
+	};
+};
+
+var Item = function(_id, _x, _y, _type) {
+	return {
+		id: _id,
+		x: _x,
+		y: _y,
+		type: _type,
+		counter: 0
+	};
+};
+
+module.exports = {
+	Player: Player,
+	Enemy: Enemy,
+	Item: Item
+};
--- Yanagi-ni-Tsubame/models/socketio.js
+++ Yanagi-ni-Tsubame/models/socketio.js
@@ -1,13 +1,43 @@
-var socket = require("socket.io");
+var socketio = require("socket.io");
+var go = require("./gameObject");
+var session = require("express-session").Session;
+var COOKIE_SECRET = "secret-session";
+var COOKIE_KEY    = "sid";
+
+
+var players = [];
+var enemys = [];
+var items = [];
 
 function socketio (server) {
-	var io = socket.listen(server);
-
+	var io = socketio.listen(server);
+	io.use(function　(socket, next) {
+		var cookie = require("cookie").parse(socket.request.headers.cookie);
+		cookie = require('cookie-parser/lib/parse').signedCookies(cookie, COOKIE_SECRET);
+		var sessionID = cookie[COOKIE_KEY];
+		console.log(sessionID);
+	});
 	// サーバー接続処理
 	io.sockets.on('connection', function(socket) {
+		var p = new go.Player(socket.id,0,0,"");
+		players.push(p);
+		console.log(socket);
 		// 初回データ送信
-		socket.json.emit('first_message', {msg : "first_message receive!"},function () {
-			console.log("first_message transmit!");
+		socket.json.emit('first_message', { player:p, players: players, enemys: enemys, items:items });
+		
+		// 各クライアントのデータ受信
+		socket.json.on("client_data", function (data) {
+
+		});
+
+		// 切断処理
+		socket.on("disconnect", function() {
+			for(var i = 0; i < players.length; i++) {
+				if(players.id === socket.id) {
+					players.splice(i,1);
+					break;
+				}
+			}
 		});
 	});
 
