8b66ec6d2c8eae88017bef235695a44ad5507a93
ほつれ <cerceris326@gmail.com>
2014-11-28 14:50:43 +0900

    Bullet.jsの追加

--- Yanagi-ni-Tsubame/public/javascripts/Player.js
+++ Yanagi-ni-Tsubame/public/javascripts/Player.js
@@ -63,60 +63,6 @@ var Player = function(GAME, data) {
 
 	GAME.scene.add(this.mesh);
 
-	// 弾定義
-	function Bullet(x, y, angle, hue) {
-		this.speedX = 6 * Math.cos(angle);
-		this.speedY = 6 * Math.sin(angle);
-		this.counter = 0;
-		this.type = "y";
-		this.halfSize = 4;
-		this.tsubame = new THREE.Object3D();
-		this.mesh = new THREE.Object3D();
-
-		var dateNow = Date.now().toString();
-		this.id = (dateNow.substring(dateNow.length - 7) + (Math.random() * 100)) | 0;
-
-		var color = new THREE.Color();
-		color.setHSL(hue, 0.6, 0.5);
-		var g = new THREE.BoxGeometry(4, 8, 0.5);
-		var m = new THREE.MeshLambertMaterial({color : color});
-		var plate = new THREE.Mesh(g, m);
-
-		var wing = new THREE.Object3D();
-		wing.add(plate);
-
-		plate = plate.clone();
-		plate.position.z = 2;
-		plate.position.y = -3;
-		wing.add(plate);
-
-		plate = plate.clone();
-		plate.position.z = -2;
-		plate.position.y = 3;
-		wing.add(plate);
-
-		wing.position.z = 3;
-		wing.position.y = -1;
-		this.tsubame.add(wing);
-
-		wing = wing.clone();
-		wing.position.z = -3;
-		wing.rotation.y = Math.PI;
-		this.tsubame.add(wing);
-		this.mesh.add(this.tsubame);
-		this.mesh.position.set(x, y, 0);
-		this.mesh.rotation.z = angle - Math.PI / 2;
-	}
-
-	Bullet.prototype.getData = function() {
-		return {
-			id : this.id,
-			x : this.mesh.position.x,
-			y : this.mesh.position.y,
-			angle : this.mesh.rotation.z
-		};
-	};
-
 	// 状態更新
 	this.update = function() {
 		// ステータス情報の表示更新
